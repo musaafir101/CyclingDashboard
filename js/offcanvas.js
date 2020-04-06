cd44f42e969472feae18cf7f3d200557f3d6d3b4
musaafir101 <taha.zaheerkh@gmail.com>
2020-04-06 20:33:35 +0500

    First Commit

---
+++ js/offcanvas.js
@@ -0,0 +1,24 @@
+/* Let's Build: With JavaScript - Web-Crunch.com
+   Subscribe on YouTube - https://youtube.com/c/webcrunch
+   
+   Let's Build: Off Canvas Menu 
+*/
+
+const menuOpen = document.querySelector(".top-nav .menu-open");
+const menuClose = document.querySelector(".top-nav .menu-close");
+const menuWrapper = document.querySelector(".top-nav .menu-wrapper");
+const topBannerOverlay = document.querySelector(".top-banner-overlay");
+
+function toggleMenu() {
+  menuOpen.addEventListener("click", () => {
+    menuWrapper.classList.add("is-opened");
+    topBannerOverlay.classList.add("is-moved");
+  });
+
+  menuClose.addEventListener("click", () => {
+    menuWrapper.classList.remove("is-opened");
+    topBannerOverlay.classList.remove("is-moved");
+  });
+}
+
+toggleMenu();
